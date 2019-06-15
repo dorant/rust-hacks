589fec47bd8ced58264181ef9070d4bc7feff752
Björn Svensson <bjorn.d.svensson@gmail.com>
2019-06-15 21:06:12 +0200

    Simple app that creates a window

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rust-hacks
+
+sdl-window: Open window with SDL, enable OpenGL
---
+++ sdl-window/src/main.rs
@@ -0,0 +1,43 @@
+extern crate gl;
+extern crate sdl2;
+
+fn main() {
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let gl_attr = video_subsystem.gl_attr();
+
+    // Setup GL: Core 4.1
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 1);
+
+    let window = video_subsystem
+        .window("Game", 900, 700)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
+
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl =
+        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    // Set blue as clear color
+    unsafe {
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'main,
+                _ => {}
+            }
+        }
+        // Clear with clear-color
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        window.gl_swap_window();
+    }
+}
