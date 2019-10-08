4fe5c92f36951625a200abf2819414ec77b847f9
Björn Svensson <bjorn.d.svensson@gmail.com>
2019-10-08 10:42:26 +0200

    Add Helm chart for simple-http-server

--- simple-http-server/README.md
+++ simple-http-server/README.md
@@ -1,7 +1,23 @@
 # Simple test of a Rust http-server
 
 ## Build Docker image
-docker build -t http-server:latest .
+docker build -t bjornsv/simple-http-server:latest .
+
+## Push image
+docker push bjornsv/simple-http-server:latest
 
 ## Test Docker image
 docker run -d -p 8080:8080 http-server:latest
+
+## Install in K8s
+cd charts
+[ helm init ]
+helm upgrade --install simple-http-server charts/simple-http-server
+
+## Example of modifications when using an Ingress instead of LB
+helm upgrade --install \
+  --set service.type="NodePort" \
+  --set service.nodePort=30600 \
+  --set message="Hello service" \
+  --set livenessProbe=null \
+ simple-http-server charts/simple-http-server
--- simple-http-server/src/main.rs
+++ simple-http-server/src/main.rs
@@ -1,31 +1,42 @@
 extern crate futures;
 extern crate hyper;
+extern crate hostname;
 
 #[macro_use]
 extern crate serde_json;
 
 use hyper::service::service_fn_ok;
-use hyper::{Body, Method, Response, Server, StatusCode};
+use hyper::{Body, Method, Response, Server};
+use hostname::get_hostname;
 
 // This is here because we use map_error.
 use futures::Future;
 
+use std::env;
+
 fn main() {
+    let addr = "0.0.0.0:8080".parse().unwrap();
+
     let router = || {
         service_fn_ok(|req| match (req.method(), req.uri().path()) {
             (&Method::GET, "/healthz") => {
                 Response::new(Body::from(json!({"status": "ok"}).to_string()))
             }
             (_, _) => {
-                let mut res = Response::new(Body::from("not found"));
-                *res.status_mut() = StatusCode::NOT_FOUND;
-                res
+                let msg = env::var("MESSAGE").unwrap_or_else(|_| "".to_string());
+                Response::new(Body::from(
+                    json!({"host": get_hostname().unwrap(),
+                           "message": msg}).to_string()))
+
+                // Specific status/response is needed, use:
+                // let mut res = Response::new(Body::from("not found"));
+                // *res.status_mut() = StatusCode::NOT_FOUND;
+                // res
             }
         })
     };
 
     // Setup and run the server
-    let addr = "0.0.0.0:8080".parse().unwrap();
     let server = Server::bind(&addr).serve(router);
 
     hyper::rt::run(server.map_err(|e| {
